b060b16f3b4ebf0837eb76c039e9616d36db71cd
Thaodv <thaodv.bkit@gmail.com>
2020-01-31 18:09:15 +0700

    Day 4 Class and Instance

---
+++ src/Day4ClassAndInstance.kt
@@ -0,0 +1,41 @@
+import java.util.*
+
+class Person(initialAge: Int) {
+    private var age: Int = 0
+
+    init {
+        age = initialAge
+        if (age < 0) {
+            println("Age is not valid, setting age to 0.")
+            age = 0
+        }
+    }
+
+    fun amIOld() {
+        when {
+            age < 13 -> println("You are young.")
+            age in 13..17 -> println("You are a teenager.")
+            else -> println("You are old.")
+        }
+    }
+
+    fun yearPasses() {
+        age++
+    }
+}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val t = sc.nextInt()
+    for (i in 0 until t) {
+        val age = sc.nextInt()
+        val p = Person(age)
+        p.amIOld()
+        for (j in 0..2) {
+            p.yearPasses()
+        }
+        p.amIOld()
+        println()
+    }
+    sc.close()
+}
